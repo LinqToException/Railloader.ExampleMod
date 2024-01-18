d7d4abaedb5a5330cfc9c8b09d25a68205a1dd7f
LinqToException <47725963+LinqToException@users.noreply.github.com>
2024-01-18 18:09:55 +0100

    Initial commit

---
+++ ExampleMod/EchoCommand.cs
@@ -0,0 +1,11 @@
+﻿using UI.Console;
+
+namespace ExampleMod
+{
+    [ConsoleCommand("/echo", "Echo echo echo echo")]
+    public class EchoCommand : IConsoleCommand
+    {
+        public string Execute(string[] components)
+            => string.Join(" ", components);
+    }
+}
---
+++ ExampleMod/ExampleMod.cs
@@ -0,0 +1,44 @@
+﻿using Railloader;
+using Serilog;
+using UI.Builder;
+
+namespace ExampleMod
+{
+    public class ExampleMod : PluginBase, IUpdateHandler, IModTabHandler
+    {
+        ILogger logger = Log.ForContext<ExampleMod>();
+
+        static ExampleMod()
+        {
+            Log.Information("Hello! Static Constructor was called!");
+        }
+
+        public ExampleMod(IModdingContext moddingContext, IModDefinition self)
+        {
+            logger.Information("Hello! Constructor was called for {modId}/{modVersion}!", self.Id, self.Version);
+
+            moddingContext.RegisterConsoleCommand(new EchoCommand());
+        }
+
+        public override void OnEnable()
+        {
+            logger.Information("OnEnable() was called!");
+        }
+
+        public void Update()
+        {
+            logger.Verbose("UPDATE()");
+        }
+
+        public void ModTabDidOpen(UIPanelBuilder builder)
+        {
+            logger.Information("Daytime!");
+            builder.AddLabel("Hello World!");
+        }
+
+        public void ModTabDidClose()
+        {
+            logger.Information("Nighttime...");
+        }
+    }
+}
